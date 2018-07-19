409cb062e0fd9b17479f66b888f08d5916e5bc12
Yomaira Escano <yjescano@gmail.com>
2018-07-19 15:51:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,5 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.floor(Math.random() * 20) + 1;
+
+let randomInteger = Math.round(randomNumber);
